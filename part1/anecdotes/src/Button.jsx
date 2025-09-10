3824841354069763ce55700b13cd1eecc42d3d5b
shiriweb <shirisha.singh36@gmail.com>
2025-09-10 13:18:05 +0545

    feat: part1 exercises complete

---
+++ part1/anecdotes/src/Button.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const Button = ({ onClickFunc, label }) => {
+  return (
+    <>
+      <button onClick={onClickFunc}>{label}</button>
+    </>
+  );
+};
+export default Button;
