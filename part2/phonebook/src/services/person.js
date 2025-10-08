50de5f85dcacbed3ae47b63d261f50746709741a
shiriweb <shirisha.singh36@gmail.com>
2025-10-08 13:10:10 +0545

    feat: added the functionality like (add delete update)

--- part2/phonebook/src/services/person.js
+++ part2/phonebook/src/services/person.js
@@ -1,15 +1,17 @@
 import axios from 'axios';
 const baseUrl = '/api/persons';
 
-//to use get method to read
+
+// Added the functionality to the app
+//used get method to read
 const getAll = () => axios.get(baseUrl);
 
 const create = newPerson => axios.post(baseUrl, newPerson);
 
-//to use put method to update
+//used put method to update
 const update = (id, newObject) => axios.put(`${baseUrl}/${id}`, newObject);
 
-//to use delete method to remove
+//used  delete method to remove
 const remove = id => axios.delete(`${baseUrl}/${id}`);
 
 export default { getAll, create, update, remove };
