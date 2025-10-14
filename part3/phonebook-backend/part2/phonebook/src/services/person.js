91af52d5531bac0eb30186e121b8354d178c20da
shiriweb <shirisha.singh36@gmail.com>
2025-10-14 11:42:19 +0545

    featt: part3

---
+++ part3/phonebook-backend/part2/phonebook/src/services/person.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+
+const baseUrl = 'http://localhost:3001/api/persons'
+
+// Get all persons
+const getAll = () => axios.get(baseUrl).then(res => res.data)
+
+// Create a new person
+const create = newPerson => axios.post(baseUrl, newPerson).then(res => res.data)
+
+// Delete a person by id
+const remove = id => axios.delete(`${baseUrl}/${id}`)
+
+// Update a person by id
+const update = (id, updatedPerson) =>
+  axios.put(`${baseUrl}/${id}`, updatedPerson).then(res => res.data)
+
+export default { getAll, create, remove, update }
