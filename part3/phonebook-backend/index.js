0933d673d812760bc18d10ae810f71046679903c
shiriweb <shirisha.singh36@gmail.com>
2025-10-14 12:28:34 +0545

    feat: part3 exercise completed

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -105,9 +105,7 @@ const errorHandler = (error, req, res, next) => {
   console.error(error.message);
   if (error.name === "CastError") {
     return res.status(400).send({ error: "malformatted id" });
-  }
-
-  if (error.name === "ValidationError") {
+  } else if (error.name === "ValidationError") {
     return res.status(400).send({ error: error.message });
   }
   next(error);
--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -3,15 +3,28 @@ const mongoose = require("mongoose");
 
 const url = process.env.MONGODB_URL;
 
-
 mongoose
   .connect(url)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log("Error connecting:", err.message));
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    required: true,
+    minlength: 8,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number`,
+    },
+  },
 });
 
 const Person = mongoose.model("Person", personSchema);
